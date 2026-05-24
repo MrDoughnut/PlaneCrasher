4b70dc6154c49ef018cb0848146e0cfd4db375d1
MrDoughnut <MrDoughnut>
2026-05-24 18:22:27 -0400

    skinny finger

--- game.js
+++ game.js
@@ -142,7 +142,7 @@ class BouncingTriangle {
 
     contains(x, y) {
         // Adds 25 pixels of invisible clickable area on all sides of the triangle
-        const touchPadding = 100000; 
+        const touchPadding = 500; 
         const paddedHalfSize = (this.size / 2) + touchPadding;
         
         return x >= this.position.x - paddedHalfSize && x <= this.position.x + paddedHalfSize &&
